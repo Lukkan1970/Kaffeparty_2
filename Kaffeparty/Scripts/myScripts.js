f2fed83e1c29f1712b7af814f444938d98a9d8d2
Lukkan1970 <patrik.luukkonen@live.com>
2017-04-13 09:23:03 +0200

    Add project files.

---
+++ Kaffeparty/Scripts/myScripts.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="C:\Users\User\Documents\Visual Studio 2015\Projects\Kaffeparty\Kaffeparty\html/nav.html" />
+
+document.getElementById("navMenu").innerHTML =
+    '<ul>' +
+            '<li><a href="index.html">Hem</a></li>' +
+            '<li><a href="products.html">Produkter</a></li>' +
+            '<li><a href="about.html">om oss</a></li>' +
+    '</ul>';
